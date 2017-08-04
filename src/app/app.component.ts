8c5ac38551198dc9d6c19a50f5f8ea02631accd0
Bridget Vuong <bridget.vuong@gmail.com>
2017-08-03 23:49:26 -0400

    Created skeleton app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+class MenuItem {
+    constructor(public name: string, public children: MenuItem[]) {}
+}
+
+var menu: MenuItem[] = [
+  new MenuItem("Menu Item 1", [
+    new MenuItem("SubMenu Item 1", [
+      new MenuItem("SubSubMenu Item 1", []),
+      new MenuItem("SubSubMenu Item 2", [])
+    ]),
+    new MenuItem("SubMenu Item 2", [])
+  ]),
+  new MenuItem("Menu Item 2", []),
+  new MenuItem("Menu Item 3", [
+    new MenuItem("SubMenuItem 3", [])
+  ])
+];
+
 @Component({
-  selector: 'app-root',
+  selector: 'fuse-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private menu: MenuItem[] = menu;
+
+  constructor() {}
 }
